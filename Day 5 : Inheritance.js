ed2094772ae3c8fcaca5057b86e490dbc4b991c8
Deepesh <119159930+deepeshovercoder@users.noreply.github.com>
2024-02-17 17:50:29 +0530

    Create Day 5 : Inheritance.js

---
+++ Day 5 : Inheritance.js
@@ -0,0 +1,11 @@
+Rectangle.prototype.area = function(){
+    return this.w * this.h;
+}
+/*
+ * Create a Square class that inherits from Rectangle and implement its class constructor
+ */
+class Square extends Rectangle{
+    constructor(a) {
+        super(a, a);
+    }
+}
