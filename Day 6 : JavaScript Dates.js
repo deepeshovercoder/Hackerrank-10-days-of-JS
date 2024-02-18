d59ec7f734bc4b9aaf1731771a420dec379a08e7
Deepesh <119159930+deepeshovercoder@users.noreply.github.com>
2024-02-18 20:54:08 +0530

    Create Day 6 : JavaScript Dates.js

---
+++ Day 6 : JavaScript Dates.js
@@ -0,0 +1,9 @@
+// The days of the week are: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+function getDayName(dateString) {
+    let dayName;
+    // Write your code here
+    let dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let date = new Date(dateString);
+    dayName = dayNames[date.getDay()];
+    return dayName;
+}
